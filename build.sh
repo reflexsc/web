5588e3a07ff630b5bbea045b66523e79a7b297cb
s3tqr2w <brandon.gillespie@kuali.co>
2016-10-22 07:59:14 -0600

    better build

--- build.sh
+++ build.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+root=/app/web/reflex.cold.org
+
+cd $root/repo
 git clean -xdf
 git pull
 /home/svc-user/bin/jekyll build
+
+cd $root
+
+date=$(date +%y-%m-%d.%H.%M.%S)
+mv stage $date
+rm -rf live
+ln -s $date live
+
+perl -e '
+	opendir(DIR, ".");
+	@all = ();
+	for my $f (readdir(DIR)) {
+		if ($f =~ /^\d+-\d+-\d+\.\d+\.\d+\.\d+$/) {
+			push(@all, $f);
+		}
+	}
+	@all = sort(@all);
+	while (@all > 2) {
+		my $last = shift(@all);
+		print("removing old $last\n");
+		system("rm", "-rf", $last);
+	}
+'
